69a230f23dd9413a5cf56dbae5bdd2469a844ff8
Anatoliy <Astarskiy@yandex.ru>
2022-08-03 18:29:41 +0300

    обновленный дизайн

--- js/script.js
+++ js/script.js
@@ -1,19 +1,62 @@
-const hamburger = document.querySelector(".hamburger"),
-  menu = document.querySelector(".menu"),
-  closeElem = document.querySelector(".menu__close");
+// menu
 
-hamburger.addEventListener("click", () => {
-  menu.classList.add("active");
+const hamburger = document.querySelector('.hamburger');
+const menu = document.querySelector('.menu');
+const closeMenu = document.querySelector('.menu__close');
+const menuLink = document.querySelectorAll('.menu__link');
+
+hamburger.addEventListener('click', () => {
+    menu.classList.add('active');
 });
 
-closeElem.addEventListener('click', () => {
+closeMenu.addEventListener('click', () => {
     menu.classList.remove('active');
-})
+});
+
+menuLink.forEach(item => {
+    item.addEventListener('click', () => {
+        menu.classList.remove('active');
+    });
+});
+
+// scroll
+const smoothLinks = document.querySelectorAll('a[href^="#"]');
+for (let smoothLink of smoothLinks) {
+    smoothLink.addEventListener('click', function (e) {
+        e.preventDefault();
+        const id = smoothLink.getAttribute('href');
+
+        document.querySelector(id).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+    });
+};
+
+// buttons 
+
+const btns = document.querySelectorAll('.promo__link');
+
+btns[0].addEventListener('click', () => {
+    if (!btns[0].classList.contains('btn_active')) {
+        btns[0].classList.add('btn_active');
+        btns[1].classList.remove('btn_active');
+    }
+});
+
+btns[1].addEventListener('click', () => {
+    if (!btns[1].classList.contains('btn_active')) {
+        btns[1].classList.add('btn_active');
+        btns[0].classList.remove('btn_active');
+    }
+});
+
+// skills
 
 
-const counters = document.querySelectorAll('.skills__ratings-counter'),
-      lines = document.querySelectorAll('.skills__ratings-line span');
+const counters = document.querySelectorAll('.skills__ratings-counter');
+const lines = document.querySelectorAll('.skills__ratings-line span');
 
-counters.forEach( (item, i) => {
+counters.forEach((item, i) => {
     lines[i].style.width = item.innerHTML;
 });
